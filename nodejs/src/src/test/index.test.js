b94e6743d37cc2d397da3e931e0c973ab2f99379
nguyenmit2012@gmail.com <nguyenmit2012@gmail.com>
2020-07-21 15:37:15 +0700

    update test file

--- nodejs/src/src/test/index.test.js
+++ nodejs/src/src/test/index.test.js
@@ -17,7 +17,7 @@ it('array has 1 on it', () => {
   expect(array).toContain(1);
 });
 
-const array = [1, 2, 10, 1000];
-it('array has 2 on it', () => {
-  expect(array).toContain(2);
-});
+// const array = [1, 2, 10, 1000];
+// it('array has 2 on it', () => {
+//   expect(array).toContain(2);
+// });
